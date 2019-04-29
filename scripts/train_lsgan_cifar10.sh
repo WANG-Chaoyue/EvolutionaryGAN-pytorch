38eb04257a1f2dd9b264e0048b8221bba9004b7e
Chaoyue Wang <chaoyue.wang@outlook.com>
2019-04-29 17:22:31 +1000

    scripts

--- scripts/train_lsgan_cifar10.sh
+++ scripts/train_lsgan_cifar10.sh
@@ -1,12 +1,11 @@
 set -ex
-python train.py --name egan_cifar10 \
-       --dataset_mode torchvision --batch_size 32 --eval_size 256 --dataroot None \
-       --model egan \
+python train.py --name lsgan_cifar10 \
+       --dataset_mode torchvision --batch_size 32 --dataroot None \
+       --model two_player_gan \
        --gpu_ids 0 \
        --download_root ./datasets/cifar10 --dataset_name CIFAR10 \
        --crop_size 32 --load_size 32 \
-       --d_loss_mode vanilla --g_loss_mode nsgan vanilla lsgan --which_D S \
-       --lambda_f 0.05 --candi_num 1 --z_type Uniform --z_dim 100 \
+       --d_loss_mode lsgan --g_loss_mode lsgan --which_D S \
        --netD DCGAN_cifar10 --netG DCGAN_cifar10 --ngf 128 --ndf 128 --g_norm none --d_norm batch \
        --init_type normal --init_gain 0.02 \
        --no_dropout --no_flip \
